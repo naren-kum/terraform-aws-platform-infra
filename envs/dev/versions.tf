e9ba83f25bd9b920c49ed33ef79ec9a5213ae31c
Naren Kumar <naren.kumar@isb-global.com>
2026-06-15 16:01:45 +0100

    Add S3 remote backend with state locking

--- envs/dev/versions.tf
+++ envs/dev/versions.tf
@@ -1,5 +1,12 @@
 terraform {
-  required_version = ">= 1.6.0"
+  required_version = ">= 1.10.0"
+
+  backend "s3" {
+    bucket       = "naren-terraform-state-361769572669-eu-west-2"
+    key          = "envs/dev/terraform.tfstate"
+    region       = "eu-west-2"
+    use_lockfile = true
+  }
 
   required_providers {
     aws = {
